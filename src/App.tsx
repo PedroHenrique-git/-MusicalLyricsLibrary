520c217983659a8f8ebefd720b6ec1068f4b7704
PedroHenrique-git <pedromf34@outlook.com>
2021-07-09 21:29:10 -0300

    trocando para hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Routes from './routes/Routes';
 
 export default function App(): JSX.Element {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes />
-        </BrowserRouter>
+        </HashRouter>
     );
 }
